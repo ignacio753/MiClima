872ac21f95234cba52868901724a85015556e4c7
Ignacio Monge <ignaciomonge@ignacios-macbook-pro.local>
2016-05-24 20:15:58 -0600

    Initial Commit

---
+++ MiClima/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiClima
+//
+//  Created by Ignacio Monge on 5/24/16.
+//  Copyright © 2016 IMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiClima/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiClima
+//
+//  Created by Ignacio Monge on 5/24/16.
+//  Copyright © 2016 IMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
